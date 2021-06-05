a77da67de532c06595751845a0d3f50bf817aebe
Victor Cardel <37451164+vscardel@users.noreply.github.com>
2021-06-04 22:23:53 -0300

    Add files via upload

--- master_server.py
+++ master_server.py
@@ -4,6 +4,13 @@ import threading
 import datetime
 import socket
 import time
+
+#descobrir como funciona a sincronização exatamente
+#criar uma thread que envia o valor de ntp de tempos em tempos para os quatro processos
+#os processos vao ficar rodando berkeley atualizando seus relogios internos da seguinte maneira:
+    #toda vez que eles trocarem figurinha, o relogio interno eh a media dos relogios
+    #toda vez que um processo recebe o valor do npt do processo mágico ele compara o valor do ntp com o relogio
+    #e ajusta caso seja necessário
   
   
 # datastructure used to store client address and clock data
@@ -19,24 +26,38 @@ port_address = [
 
 ]
 
+sockets_clientes_dict = {
+    0:None,
+    1:None,
+    2:None,
+    3:None
+}
+
+
 ''' nested thread function used to receive 
     clock time from a connected client '''
+
+
+#modificar
 def startRecieveingClockTime(connector, address):
   
     while True:
         # recieve clock time
-        clock_time_string = connector.recv(1024).decode()
+        rcv_string = connector.recv(1024).decode()
+        clock_time_string = rcv_string.split('*')[0]
+        sender_process_number = rcv_string.split('*')[1]
+
         clock_time = parser.parse(clock_time_string)
         clock_time_diff = datetime.datetime.now() - \
                                                  clock_time
-  
-        client_data[address] = {
+        
+        client_data[sender_process_number] = {
                        "clock_time"      : clock_time,
                        "time_difference" : clock_time_diff,
                        "connector"       : connector
                        }
   
-        print("Client Data updated with: "+ str(address),
+        print("Dados dos clientes atualizados no endereço: "+ str(address),
                                               end = "\n\n")
         time.sleep(5)
   
@@ -59,21 +80,22 @@ def acceptConnection(master_server):
                                            slave_address, ))
         current_thread.start()
 
-def requestConnection(client_socket):
-    while True:
-        # provide server with clock time at the client
+def requestConnection(sockets_clientes_dict):
 
-        # ntp_client = ntplib.NTPClient()
-        # ntp_response = ntp_client.request('south-america.pool.ntp.org',version=3)
-        # curr_time = time.process_time_ns()
-        # if abs(curr_time, ntp_response) > K:
-        #   curr_time = ntp_response
-        
-        client_socket.send(str(
-                       datetime.datetime.now()).encode())
+    while True:
+
+        for processo in sockets_clientes_dict:
+            #processos clientes
+            clientes = sockets_clientes_dict[processo]
+
+            #ideia: so mandar o ntp em vez de datetime
+            for socket_cliente in clientes:
+                #mandar uma string que tenha o numero do processo junto
+                #usando '*' como separador
+                send_string = str(datetime.datetime.now()) + '*' + str(processo)
+                socket_cliente.send(send_string.encode())
+                print('tempo recente do processo ' + str(processo) + " enviado com sucesso\n")
   
-        print("Recent time sent successfully",
-                                          end = "\n\n")
         time.sleep(5)
   
   
@@ -98,6 +120,9 @@ def getAverageClockDiff():
   
 ''' master sync thread function used to generate 
     cycles of clock synchronization in the network '''
+
+#ela age a parte das outras checando os dados de client data
+#precisamos descobrir como reenviar os tempos para todos os outros processos
 def synchronizeAllClocks():
   
     while True:
@@ -137,25 +162,38 @@ def synchronizeAllClocks():
 # function used to initiate the Clock Server / Master Node
 def initiateClockServer(socket_list):
 
-    for skt in socket_list:
+    for i in range(len(socket_list)):
 
-        curr_port_number = skt.getsockname()[1]
+        sockets_clientes_i = []
 
-        client_socket = socket.socket()           
-        client_socket.connect(('127.0.0.1', curr_port_number))
+        for j in range(len(socket_list)):
 
-        request_thread = threading.Thread(
-                            target = requestConnection,
-                            args = (client_socket,) )
+            if i != j:
+                #numero da porta dos sockets dos outros processos
+                port_number = socket_list[j].getsockname()[1]
+                client_socket = socket.socket()  
+                client_socket.connect(('127.0.0.1', port_number))
+                sockets_clientes_i.append(client_socket)
 
-        request_thread.start()
+        sockets_clientes_dict[i] = sockets_clientes_i
 
 
-    # start synchroniztion
+    #passa os sockets servidores para se conectar um com o outro
+    request_thread = threading.Thread(
+                      target = requestConnection,
+                      args = (sockets_clientes_dict,) )
+
+    request_thread.start()
+
+
+    #agora precisamos entender isso aqui
+
+    #começa a sincronizacao
+
     print("Starting synchronization parallely...\n")
     sync_thread = threading.Thread(
-                          target = synchronizeAllClocks,
-                          args = ())
+                        target = synchronizeAllClocks,
+                        args = ())
     sync_thread.start()
   
   
@@ -197,5 +235,10 @@ if __name__ == '__main__':
         master_thread.start()
 
         socket_list.append(socket_escuta)
+
+        #cada socket em socket_list esta habilitado a aceitar as requisicoes,
+        #e ao aceitar, receber os tempos de relogio e guardar os dados em
+        #client_data
+
 
     initiateClockServer(socket_list)
